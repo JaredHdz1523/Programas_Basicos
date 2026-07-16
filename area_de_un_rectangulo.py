5b2cc6b001460ea9e9145e572524d67abac6a0af
JaredHdz1523 <26150485@aguascalientes.tecnm.mx>
2026-07-16 14:52:14 -0600

    programa de area de un triangulo

---
+++ area_de_un_rectangulo.py
@@ -0,0 +1,6 @@
+#CALCULAR EL AREA DE UN TRIANGULO
+# Programa para calcular el área de un rectángulo
+base = float(input("Ingrese la base: "))
+altura = float(input("Ingrese la altura: "))
+area = base * altura
+print("El área del rectángulo es:", area)
