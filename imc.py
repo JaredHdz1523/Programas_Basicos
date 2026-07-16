542dfdc2d8c3a4e9c0aca71c2b0d9c2d0cd6f08e
JaredHdz1523 <26150485@aguascalientes.tecnm.mx>
2026-07-16 15:17:02 -0600

    Programa para calcular el peso

--- imc.py
+++ imc.py
@@ -1,6 +1,3 @@
-peso = int(input("Dime tu peso en kg: "))
-altura = int(input("Dime tu altura en cm: "))
-print("Tu IMC aproximado es:", (peso * 10000) // (altura ** 2))
 peso = float(input("Peso en kg: "))
 altura = float(input("Altura en m: "))
 imc = peso / (altura ** 2)
